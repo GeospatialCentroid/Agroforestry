68115cc1bd061fcb15aa7a8ae31f830b14c73666
Dan Carver <32108872+dcarver1@users.noreply.github.com>
2024-05-28 18:09:44 -0600

    generate a mask only validation layer

--- scripts/applyingMasksToValidationAreas.R
+++ scripts/applyingMasksToValidationAreas.R
@@ -6,6 +6,9 @@ library(stringr)
 nlcdMasks <- list.files("data/processed/nlcd",pattern = ".gpkg", full.names = TRUE, recursive = TRUE)
 # tccs <- nlcdMasks[grepl(pattern = "tcc", nlcdMasks)]
 forests <- nlcdMasks[grepl(pattern = "forest", nlcdMasks)]
+# urban areas 
+urbanFiles <- list.files("data/raw/censusData/", pattern = "*\\.shp", full.names = TRUE, recursive = TRUE )
+urbanFiles2 <- urbanFiles[str_ends(string = urbanFiles, pattern = ".shp")]
 
 # set year 
 year <- 2020
@@ -36,25 +39,47 @@ if(!file.exists(fileName1)){
                      overwrite = TRUE)
   }
 }
-# forest mask layer 
+
+
+
+# forest mask layer and city mask 
 for(i in 1:length(validationFiles)){
   r1 <- validationFiles[i]
   print(r1)
   # pull name for reference 
   name1 <- stringr::str_remove(basename(r1), pattern = ".tif")
   fileName2 <- paste0("data/products/samplingAreas/",year,"/rendered/",name1,"_Masked.tif")
+  fileName3 <- paste0("data/products/samplingAreas/",year,"/rendered/",name1,"_Mask_Only.tif")
+  
+  u1 <- urbanFiles2[grepl(pattern = year, urbanFiles2)]
   if(!file.exists(fileName2)){
+    # read in forest data and reprojects
   r2 <- terra::rast(r1) |>
     terra::project(forestVal)
+    # read in urban data and reproject 
+  u2 <- terra::vect(u1)|>
+    terra::project(forestVal)|>
+    terra::crop(r2)
+  if(nrow(u2)!=0){
+    print("removing town")
+    r2 <- r2 |>
+      terra::mask(u2, inverse = TRUE,updatevalue=2)
+  }
+
   # crop tcc 
   forest2 <-  forestVal |>
     terra::crop(r2)
   # mask 
   r3 <- r2 |>
     terra::mask(forest2, inverse = TRUE,updatevalue=2)
-  # export 
   terra::writeRaster(r3, filename = fileName2,
                      overwrite = TRUE)
+  # export 
+  r4 <- ifel(r3 <= 1, NA , 1)
+  #export the mask layer 
+  terra::writeRaster(r4, filename = fileName3,
+                     overwrite = TRUE)
+  
   }
 }
 
