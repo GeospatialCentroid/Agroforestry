c88ebaf9dcc54e5bd9545319f21ed8f7847b06a9
dcarver1 <carver.dan1@gmail.com>
2024-06-20 16:29:37 -0700

    integrated the riparian mask

--- scripts/compileModelResults.R
+++ scripts/compileModelResults.R
@@ -10,6 +10,20 @@ images <- googledrive::drive_ls(path = "agroforestry",pattern = ".tif")  |>
   dplyr::filter(!grepl('validationGrid', name))|>
   dplyr::filter(!grepl('naipGrid', name))
   
+#export the riparian image 
+# riparian <- images[images$name == "nebraskaRiparian.tif", ]
+# googledrive::drive_download(as_id(riparian$id),
+#                             path = paste0("data/raw/riparian/riparianArea10.tif"))
+# riparianRast30 <- terra::rast("data/raw/riparian/riparianArea30.tif")
+# 
+# r30proj <- riparianRast30 |>
+#   terra::project("+init=EPSG:4326")
+# terra::writeRaster(r30proj, filename = "data/products/riparian/nebraskaRiparian30.tif")
+# 
+# r10proj <- terra::rast("data/raw/riparian/riparianArea10.tif")|>
+#   terra::project("+init=EPSG:4326")
+# terra::writeRaster(riparianRast10 , filename = "data/products/riparian/nebraskaRiparian10.tif")
+
 # images2020 <- filteredImages |> dplyr::filter(!grepl('2020', name))
 # images2016 <- filteredImages |> dplyr::filter(!grepl('2016', name))
 # images2010 <- filteredImages |> dplyr::filter(!grepl('2010', name))
@@ -123,6 +137,7 @@ forests <- nlcdMasks[grepl(pattern = "forest", nlcdMasks)]
 urbanFiles <- list.files("data/products/censusData/", pattern = "*\\.shp", full.names = TRUE, recursive = TRUE )
 urbanFiles2 <- urbanFiles[stringr::str_ends(string = urbanFiles, pattern = ".shp")]
 # riparian zones 
+riparian <- terra::vect("data/raw/test.shp")
 ## need to get this cropped to nebraska and out of the GDB file structure 
 # st_layers("data/raw/Data/RiparianAreas.gdb")
 # rp <- sf::st_read("data/raw/Data/RiparianAreas.gdb", layer = "fras_blk_usfs_riparian_areas_1")
@@ -156,7 +171,7 @@ generateFinalGridImages <- function(year, modelGrids, forests, urbanFiles2){
   # select all unique grids 
   ids <- grids$Unique_ID
   # itorate over grids to produce outputs 
-  for(i in ids[1:20]){
+  for(i in ids){
     allImages <- models[grepl(paste0("/",i,"_"), models)]
     gridName <- i 
     unmaskedPath <- paste0("data/products/models",year,"/fullImages/",gridName,"_fullUnMasked.tif")
@@ -223,97 +238,43 @@ generateFinalGridImages <- function(year, modelGrids, forests, urbanFiles2){
   }
 }
 
-generateFinalGridImages(year = year, 
-                        modelGrids = modelGrids,
-                        forests = forests, 
-                        urbanFiles2 = urbanFiles2)
 
 
+generateFinalGridImages(year = "2010", 
+                          modelGrids = modelGrids,
+                          forests = forests, 
+                          urbanFiles2 = urbanFiles2)
 
-models2010 <- list.files("data/products/models2010/grids", full.names = TRUE)
-# test <- terra::rast(models2010[1])
- grid2010 <- terra::vect(modelGrids[1])
+# applied the riparian area mask 
+year = "2010"
+riparianData = terra::rast("data/products/riparian/nebraskaRiparian30.tif")
 
-# Select the year 
-forest2010 <- terra::vect(forests[grepl(pattern = "2010", x = forests)]) |>
-  terra::project("+init=EPSG:4326")
-urban2010 <- terra::vect(urbanFiles2[grepl(pattern = "2010", x = urbanFiles2)])|>
-  terra::project("+init=EPSG:4326")
-
-# itorate over the gridid 
-year <- "2010"
-ids <- grid2010$Unique_ID
-i <- "X12-101"
-# aggregate all images from a grid 
-allImages <- models2010[grepl(paste0("/",i,"_"), models2010)]
-
-# select all non b models 
-origImages <- allImages[!grepl(pattern = "_b_", allImages )]
-
-
-
-## reclass binary maps when two images exist 
-## merge images if multiple grids exists 
-for(i in seq_along(origImages)){
-  print(origImages[i])
-  n1 <- basename(origImages[i]) |>
-    str_split(pattern = "_") |> 
-    unlist()
-  gridName <- n1[1]
-  n2 <- n1[3]
-  # select all paths with this image name 
-  r1 <- allImages[grepl(pattern = n2, x = allImages)]
-  # generate rast object 
-  r2 <- mergeClass(r1)
-  if(i == 1){
-    r3 <- r2
-  }else{
-    print(paste0("merging ", i))
-    r3 <- merge(r3,r2)
+applyRiparianMask <- function(year,riparianData){
+  
+  # set the based path for images 
+  base <- paste0("data/products/models",year)
+  
+  # pull all masked images 
+  files <- list.files(
+    path = paste0(base, "/maskedImages"),
+    full.names = TRUE,
+    pattern = ".tif"
+  )
+  for(i in files[1:3]){
+    print(i)
+    image <- terra::rast(i)
+    # crop riparian layer 
+    r30 <- terra::crop(x = riparianData,
+                       y = image) |>
+      terra::as.polygons()
+    # apply the mask 
+    combined <- terra::mask(image, r30, inverse = TRUE,updatevalue=2)
+    # mask to the origin image
+    image2 <- ifel(image == 1, 1 , NA)
+    c2 <- combined * image2
+    #export the image 
+    writeRaster(x = c2, filename = paste0(base,"/maskedWithRiparian/",names(image), "_riparianClass.tif"),
+                overwrite = TRUE)
   }
 }
-# set the name of the object 
-rastName <-  paste0(gridName,"_",year)
-names(r3) <-rastName
-# export 
-terra::writeRaster(x = r3, filename = paste0("data/products/models",year,"/fullImages/",gridName,"_fullUnMasked.tif"))
-
-
-#  nlcd tree mask 
-## crop 
-f2 <-  forest2010 |>
-  crop(r3)
-
-if(nrow(f2)){
-  r4 <- r3 |>
-    terra::mask(f2, inverse = TRUE,updatevalue=NA)
-}else{
-  r4 <- r3
-}
-
-# town mask 
-t2 <- urban2010 |> 
-  crop(r3)
-if(nrow(t2)!=0){
-  print("removing town")
-  r4 <- r4 |>
-    terra::mask(t2, inverse = TRUE,updatevalue=NA)
-}
-# export the masked image 
-terra::writeRaster(x = r4, filename = paste0("data/products/models",year,"/maskedImages/",gridName,"_Masked.tif"))
-
-# riparian classification 
-## repreat this process with the town mask segment as well. 
-# town mask 
-# t2 <- urban2010 |> 
-#   crop(r3)
-# if(nrow(t2)!=0){
-#   print("removing town")
-#   r4 <- r4 |>
-#     terra::mask(t2, inverse = TRUE,updatevalue=NA)
-# }
-
-
-
-
-
+  
