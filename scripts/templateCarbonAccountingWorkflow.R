b87159aee43273299f8243d984208aaebf4ad9a7
dcarver1 <carver.dan1@gmail.com>
2024-10-24 15:54:36 -0700

    stated the carbon calculation process

---
+++ scripts/templateCarbonAccountingWorkflow.R
@@ -0,0 +1,137 @@
+### Agroforestry Biomass Carbon
+### 
+### Objectives: Estimate biomass carbon stock changes in trees outside of forest across Nebraska croplands and grasslands
+
+### Scrip goal: integrate TOF determined area and emission factors (source: "af_lmm_updated.R")
+### Method: IPCC-Tier 2, gain-loss framework. Simplified equation = (gain - losses) * area
+### Gains - annual C accumulation from immature (< 20 years) and mature (>= 20 years) trees.
+ # assuming that all trees in 2010 are mature 
+ # there for start with changes between 2010 and 2016
+    # if area is present in both years stays as areas 
+    # if area is present in 2010 but not 2016 == loss
+    # if area is present in 2016 but not 2010 == gain 
+### Losses - C losses from disturbance and tree harvest - no specific information - proxy: area reduction
+
+### Notes on factors: - Values are in tons of C ha-1 and ton C ha-1 yr-1
+
+### - factors vary by age, species, and climate (moisture) :: for pilot study, consider using a simplified model with only age as predictor
+
+
+
+### 
+### Input files (name files): areas and emission factors (and associated uncertainty)
+### Packages needed: 
+
+### Steps:
+###
+### 1) load areas for the 3 different periods (classes 1 to 7, which disaggregate total areas by timeseries presence/absence - proxy for determining age ### of stands and replacement) - note: a conservative assumption would be that all stands present at the beggining of the study (2010) are at mature age
+      # we need to perform this analysis based on the model grid. That is the primary iterative feature 
+### 2) estimate overall area uncertainty based on sampling error and confusion matrix  
+      # this will come form the spreadsheet online, for now we can provide some dummy numbers and assign those to the spatial model grid feature
+### 3) multiply gain and losses by the respective areas (increase or decrease) across the timeseries
+      # missing something here with the specifics? check with Gabriel 
+### 4) combine areas and gain and loss factors through a Monte Carlo simulation - test stability of the simulation for confidence interval to define nreps
+      # seem like were simulation change in the gain and loss numbers. Not clear if we need to recalculate the change over time spatial relationships or not
+### 5) generate C reports, potentially by different environments/agroforestry systems: e.g., riparian buffer, grasslands (e.g., silvopasture), and 
+### croplands (e.g., alley cropping, windbreaks/hedgerows) and biomass C changes map
+### 
+###
+###
+
+#load libraries 
+pacman::p_load(dplyr,sf, terra)
+
+
+# sources some existing functions 
+source("scripts/reclassByYear.R")
+
+# temp function for assigning a current score 
+assignScore <- function(data){
+  # get the unique model grids 
+  uniqueModels <- unique(data$modelGrid)
+  # generate a template score value 
+  scores <- runif(length(uniqueModels), min=0.1, max=0.6)
+  # assign a score value 
+  data$validationScore <- NA 
+  for(i in seq_along(uniqueModels)){
+    id <- uniqueModels[i]
+    val <- scores[i]
+    index <- data$modelGrid == id 
+    data$validationScore[index] <- val
+  }
+  return(data)
+}
+
+# required input data  ----------------------------------------------------
+## add the validation score reference 
+grids2010 <- sf::st_read("data/products/modelGrids_2010.gpkg") |>  assignScore() |> dplyr::mutate(year= "2010")
+grids2016 <- sf::st_read("data/products/modelGrids_2016.gpkg") |>  assignScore() |> dplyr::mutate(year= "2016")
+grids2020 <- sf::st_read("data/products/modelGrids_2020.gpkg") |>  assignScore() |> dplyr::mutate(year= "2020")
+grids <- grids2010 |> 
+  bind_rows(grids2016) |>
+  bind_rows(grids2020)
+  
+
+# change over time data 
+cot <- list.files(path = "data/products/changeOverTime",
+                  full.names = TRUE,
+                  pattern = ".tif")
+
+gridIndex <- grids2010$Unique_ID[10]
+cotFiles <- cot
+processingEffort <- function(grids, gridIndex, cotFiles){
+  
+  # seperate out grid 
+  selectGrids <- grids[grids$Unique_ID == gridIndex,]
+  
+  # grap error measures per each year 
+  e10 <- selectGrids$validationScore[selectGrids$year == "2010"]
+  e16 <- selectGrids$validationScore[selectGrids$year == "2016"]    
+  e20 <- selectGrids$validationScore[selectGrids$year == "2020"]
+  
+  # select specific model areas 
+  raster <- cotFiles[grepl(pattern = paste0(gridIndex,"_changeOverTime"), x = cotFiles)] |>
+    terra::rast()
+  # pull out the years 
+  r2010 <- raster |> getYearMap(year = "2010")
+  r2016 <- raster |> getYearMap(year = "2016")
+  r2020 <- raster |> getYearMap(year = "2020")
+  
+  # gain and loss 2016 
+  values2016 <- r2016 - r2010 
+  ## 0 : both predicted forest 
+  ## 1 : forest in 2016 but not in 2010 
+  ## -1 : forest in 2010 but not in 2016
+  
+  # using pixel counts 
+  ## returns a data frame with false match in row 1 and true in row 2 
+  same2016 <- freq(values2016 == 0)$count[2]
+  gain2016 <- freq(values2016 == 1)$count[2]
+  loss2016 <- freq(values2016 == -1)$count[2]
+  # or reclassify 
+  ## define changes 
+  rSame <- rbind(c(-1,NA), c(1,NA), c(0,1))
+  rGain <- rbind(c(-1,NA), c(0,NA)) # don't need the thrid because values are 1
+  rLoss <- rbind(c(1,NA), c(0,NA), c(-1,1))
+  ## run the reclass 
+  same2016 <- classify(x = values2016,rSame)
+  gain2016 <- classify(x = values2016,rGain)
+  loss2016 <- classify(x = values2016,rLoss)
+  ## if the spatial component is still needed 
+  
+  
+  ### this preps things for the calculatation measures, but I'm not 100% what those would be. 
+  
+  
+  
+  ## montecarlo 
+  
+  
+}
+
+
+
+
+
+
+
