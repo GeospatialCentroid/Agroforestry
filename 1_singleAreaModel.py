db4abab7d9ae623c3458c251ad20783bf0317e03
unknown <carver.dan1@gmail.com>
2025-02-28 17:29:58 -0700

    draft of the single image modeling workflow

--- 1_singleAreaModel.py
+++ 1_singleAreaModel.py
@@ -7,21 +7,27 @@ import geemap
 import geopandas as gpd
 import pandas as pd
 import numpy as np
+
+ee.Initialize(project='agroforestry2023')
 # from agroforestry.config import * 
 from agroforestry.geeHelpers import *
 from agroforestry.naipProcessing import *
 from agroforestry.snicProcessing import *
 from agroforestry.randomForest import *
 from agroforestry.exportFunctions import *
+from agroforestry.histMatch import *
 
-try:
-        ee.Initialize(project='agroforestry2023')
-except Exception as e:
-        ee.Authenticate(auth_mode='notebook')        
-        ee.Initialize()
+# ee.Authenticate()        
+
+
+# try:
+#         ee.Initialize(project='agroforestry2023')
+# except Exception as e:
+#         ee.Authenticate(auth_mode='notebook')        
+#         ee.Initialize()
 
 # read in grid object 
-grid = gpd.read_file("data/processed/griddedFeatures/twelve_mi_grid_uid.gpkg")
+Grids = gpd.read_file("data/processed/griddedFeatures/twelve_mi_grid_uid.gpkg")
 
 # define model grid 
 modelGrid =  "X12-519" 
@@ -39,8 +45,8 @@ bandsToUse = ["contrast_n_mean", "entropy_n_mean", "entropy_n", "entropy_g_mean"
 processedData = 'data/processed/'+modelGrid
 
 # select grids to generate spatial object 
-mGrid =  grid[grid['Unique_ID'] == modelGrid]
-aGrid =  grid[grid['Unique_ID'] == applyGrid]
+mGrid =  Grids[Grids['Unique_ID'] == modelGrid]
+aGrid =  Grids[Grids['Unique_ID'] == applyGrid]
 
 # convert to a gee object 
 mAOI = geemap.gdf_to_ee(mGrid)
@@ -76,127 +82,119 @@ rfPixelTrim = trainRFModel(bands=bandsToUse,  inputFeature=training, nTrees=nTre
 pixelValidationTrim = testRFClassifier(classifier=rfPixelTrim, testingData= testing)
 
 
-# naip processing for model grid --- no normalization at this point 
+# naip processing for model grid --- no normalization at this point so we can call from the existing function 
 # grab naip for the year of interest, filter, mask, mosaic to a single image
-naip1 = geemap.get_annual_NAIP(year).filterBounds(mAOI).mosaic() 
-# Generate NDVI 
-ndvi = naip1.normalizedDifference(["N","R"])
-# generate GLCM
-glcm_g = naip1.select('G').glcmTexture(size = windowSize).select(['G_savg','G_contrast','G_ent'],["savg_g", "contrast_g", "entropy_g"])
-glcm_n = naip1.select('N').glcmTexture(size= windowSize).select(['N_savg','N_contrast','N_ent'],["savg_n", "contrast_n", "entropy_n"])
-# add to naip 
-naip2 = naip1.addBands(glcm_g).addBands(glcm_n)
-
-# average and standard deviation NDVI
-ndvi_sd_neighborhood =  ndvi.select('nd').reduceNeighborhood(reducer = ee.Reducer.stdDev(),kernel = ee.Kernel.circle(windowSize)).rename(["nd_sd_neighborhood"])
-ndvi_mean_neighborhood =  ndvi.select('nd').reduceNeighborhood(reducer= ee.Reducer.mean(),  kernel= ee.Kernel.circle(windowSize)).rename(["nd_mean_neighborhood"])
-
-# Bind ndvi after the glcm processall the bands together 
-naip = naip2.addBands(ndvi).addBands(ndvi_sd_neighborhood).addBands(ndvi_mean_neighborhood)
-
+naipTrain = prepNAIP(aoi=mAOI,windowSize=windowSize, year=year)
+geePrint(naipTrain)
 #####
-# gather NAIP data required for apply model area 
-# grab naip for the year of interest, filter, mask, mosaic to a single image
-naip1a = geemap.get_annual_NAIP(year).filterBounds(aAOI).mosaic() 
 
-#
+# getNAIP(year=year, gridArea=mAOI)
+
+
+#### model grids that need to be reclassified 
 # integrate Gavins histogram matching work. Start byt 
+naip1 = matchSelf(gridArea= aAOI, 
+                  year = year)
+geePrint(naip1)
+
 
 # Generate NDVI 
-ndvia = naip1a.normalizedDifference(["N","R"])
+ndvia = naip1.normalizedDifference(["N","R"])
+
 # generate GLCM
-glcm_ga = naip1a.select('G').glcmTexture(size = windowSize).select(['G_savg','G_contrast','G_ent'],["savg_g", "contrast_g", "entropy_g"])
-glcm_na = naip1a.select('N').glcmTexture(size= windowSize).select(['N_savg','N_contrast','N_ent'],["savg_n", "contrast_n", "entropy_n"])
+glcm_ga = naip1.select('G').glcmTexture(size = windowSize).select(['G_savg','G_contrast','G_ent'],["savg_g", "contrast_g", "entropy_g"])
+glcm_na = naip1.select('N').glcmTexture(size= windowSize).select(['N_savg','N_contrast','N_ent'],["savg_n", "contrast_n", "entropy_n"])
 # add to naip 
-naip2a = naip1a.addBands(glcm_g).addBands(glcm_n)
+naip2a = naip1.addBands(glcm_ga).addBands(glcm_na)
 
 # average and standard deviation NDVI
 ndvi_sd_neighborhooda =  ndvia.select('nd').reduceNeighborhood(reducer = ee.Reducer.stdDev(),kernel = ee.Kernel.circle(windowSize)).rename(["nd_sd_neighborhood"])
 ndvi_mean_neighborhooda =  ndvia.select('nd').reduceNeighborhood(reducer= ee.Reducer.mean(),  kernel= ee.Kernel.circle(windowSize)).rename(["nd_mean_neighborhood"])
 
 # Bind ndvi after the glcm processall the bands together 
-naipa = naip2a.addBands(ndvia).addBands(ndvi_sd_neighborhooda).addBands(ndvi_mean_neighborhooda)
+naipa = naip1.addBands(ndvia).addBands(ndvi_sd_neighborhooda).addBands(ndvi_mean_neighborhooda)
 
+# apply the model 
+classifiedPixelsTrim = applyRFModel(imagery=naipa, bands=bandsToUse,classifier=rfPixelTrim).clip(aAOI).uint8()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-for i in years: 
-    # define file location 
-    processedData = 'data/processed/'+initGridID
-    neighborGrid = pd.read_csv(processedData + "/neighborGrids.csv")
-    grid36 = neighborGrid[neighborGrid['poisition'].isin([1,2,3,4])]
-    # set aoi for the gee objects 
-    aoiID = initGridID
-    #Define bands to use -- setting manually 
-    bandsToUse = vsurfNoCor
-    ### this was used to generate the _b versions of the models 
-    # bandsToUse = ["contrast_n_mean", "entropy_n_mean", "entropy_n", "entropy_g_mean","nd_mean_neighborhood","contrast_n","entropy_g","nd_mean","contrast_g_mean","contrast_g"] 
-    # select multiple grids level 
-    gridSelect =  grid.loc[grid.Unique_ID.isin(grid36.Unique_ID)].dissolve()
-    # convert to a gee object 
-    aoi1 = geemap.gdf_to_ee(gridSelect)
-    # import training dataset 
-    trainingData = gpd.read_file(filename="data/processed/" + str(initGridID) +"/"+ "agroforestrySamplingData_"+str(i)+".geojson") # initGridID defined int he config file
-    # divide the data into test train spilts 
-    trainingData = trainingData.sample(frac = 1)
-    # get rows 
-    total_rows = trainingData.shape[0]
-    # get train size 
-    train_size = int(total_rows*test_train_ratio)
-
-    # Split data into test and train
-    train = trainingData[0:train_size]
-    test = trainingData[train_size:]
-    # define the GEE objects
-    training = geemap.gdf_to_ee(gdf=train)
-    testing = geemap.gdf_to_ee(gdf=test)
-    # train model
-    rfPixelTrim = trainRFModel(bands=bandsToUse,  inputFeature=training, nTrees=nTrees,setSeed=setSeed )
-    ## run validation using the testing set 
-    pixelValidationTrim = testRFClassifier(classifier=rfPixelTrim, testingData= testing)
-
-    # Generate model based on year define in config 
-    # generate NAIP layer 
-    naipEE = prepNAIP(aoi=aoi1, year=i,windowSize=windowSize)
-    ##
-
-
-    # geePrint(naipEE.bandNames())
-    # normal the naip data
-    # normalizedNAIP = normalize_by_maxes(img=naipEE, bandMaxes=bandMaxes)
-    # produce the SNIC object 
-    ## filtering the image bands right away based on the single model output 
-    snicData = snicOutputs(naip = naipEE,
-                        SNIC_SeedShape = SNIC_SeedShape, 
-                        SNIC_SuperPixelSize = SNIC_SuperPixelSize, 
-                        SNIC_Compactness = SNIC_Compactness, 
-                        SNIC_Connectivity = SNIC_Connectivity,
-                        # nativeScaleOfImage = nativeScaleOfImage, 
-                        bandsToUse_Cluster = bandsToUse_Cluster).select(bandsToUse)
-    # apply the model and clip to aoi and reclass to unsigned 8bit image 
-    classifiedPixelsTrim = applyRFModel(imagery=snicData, bands=bandsToUse,classifier=rfPixelTrim).clip(aoi1).uint8()
-    demoImage = classifiedPixelsTrim #.clip(exportAOI)
-    print("projects/agroforestry2023/assets/"+ str(initGridID) + "_" + str(i) + "_36grid")
-    # export image to asset 
-    task = ee.batch.Export.image.toAsset(
-        image = demoImage,
-        description = str(initGridID) + "_" + str(i) + "_b_36grid",
-        assetId = "projects/agroforestry2023/assets/"+ str(initGridID) + "_b_" + str(i) + "_36grid",
+?    # export image to asset 
+task = ee.batch.Export.image.toAsset(
+        image = classifiedPixelsTrim,
+        description = str(applyGrid) + "_histNorm_self",
+        assetId = "projects/agroforestry2023/assets/"+ str(applyGrid) + "_histNorm_self",
         region=aoi1.geometry(),
         scale=1,
         crs= demoImage.projection(),
         maxPixels = 1e13
-    )
-    task.start()
+)
+task.start()
+
+# for i in years: 
+#     # define file location 
+#     processedData = 'data/processed/'+initGridID
+#     neighborGrid = pd.read_csv(processedData + "/neighborGrids.csv")
+#     grid36 = neighborGrid[neighborGrid['poisition'].isin([1,2,3,4])]
+#     # set aoi for the gee objects 
+#     aoiID = initGridID
+#     #Define bands to use -- setting manually 
+#     bandsToUse = vsurfNoCor
+#     ### this was used to generate the _b versions of the models 
+#     # bandsToUse = ["contrast_n_mean", "entropy_n_mean", "entropy_n", "entropy_g_mean","nd_mean_neighborhood","contrast_n","entropy_g","nd_mean","contrast_g_mean","contrast_g"] 
+#     # select multiple grids level 
+#     gridSelect =  grid.loc[grid.Unique_ID.isin(grid36.Unique_ID)].dissolve()
+#     # convert to a gee object 
+#     aoi1 = geemap.gdf_to_ee(gridSelect)
+#     # import training dataset 
+#     trainingData = gpd.read_file(filename="data/processed/" + str(initGridID) +"/"+ "agroforestrySamplingData_"+str(i)+".geojson") # initGridID defined int he config file
+#     # divide the data into test train spilts 
+#     trainingData = trainingData.sample(frac = 1)
+#     # get rows 
+#     total_rows = trainingData.shape[0]
+#     # get train size 
+#     train_size = int(total_rows*test_train_ratio)
+
+#     # Split data into test and train
+#     train = trainingData[0:train_size]
+#     test = trainingData[train_size:]
+#     # define the GEE objects
+#     training = geemap.gdf_to_ee(gdf=train)
+#     testing = geemap.gdf_to_ee(gdf=test)
+#     # train model
+#     rfPixelTrim = trainRFModel(bands=bandsToUse,  inputFeature=training, nTrees=nTrees,setSeed=setSeed )
+#     ## run validation using the testing set 
+#     pixelValidationTrim = testRFClassifier(classifier=rfPixelTrim, testingData= testing)
+
+#     # Generate model based on year define in config 
+#     # generate NAIP layer 
+#     naipEE = prepNAIP(aoi=aoi1, year=i,windowSize=windowSize)
+#     ##
+
+
+#     # geePrint(naipEE.bandNames())
+#     # normal the naip data
+#     # normalizedNAIP = normalize_by_maxes(img=naipEE, bandMaxes=bandMaxes)
+#     # produce the SNIC object 
+#     ## filtering the image bands right away based on the single model output 
+#     snicData = snicOutputs(naip = naipEE,
+#                         SNIC_SeedShape = SNIC_SeedShape, 
+#                         SNIC_SuperPixelSize = SNIC_SuperPixelSize, 
+#                         SNIC_Compactness = SNIC_Compactness, 
+#                         SNIC_Connectivity = SNIC_Connectivity,
+#                         # nativeScaleOfImage = nativeScaleOfImage, 
+#                         bandsToUse_Cluster = bandsToUse_Cluster).select(bandsToUse)
+#     # apply the model and clip to aoi and reclass to unsigned 8bit image 
+#     classifiedPixelsTrim = applyRFModel(imagery=snicData, bands=bandsToUse,classifier=rfPixelTrim).clip(aoi1).uint8()
+#     demoImage = classifiedPixelsTrim #.clip(exportAOI)
+#     print("projects/agroforestry2023/assets/"+ str(initGridID) + "_" + str(i) + "_36grid")
+#     # export image to asset 
+#     task = ee.batch.Export.image.toAsset(
+#         image = demoImage,
+#         description = str(initGridID) + "_" + str(i) + "_b_36grid",
+#         assetId = "projects/agroforestry2023/assets/"+ str(initGridID) + "_b_" + str(i) + "_36grid",
+#         region=aoi1.geometry(),
+#         scale=1,
+#         crs= demoImage.projection(),
+#         maxPixels = 1e13
+#     )
+#     task.start()
 
---
+++ agroforestry/histMatch.py
@@ -0,0 +1,258 @@
+# Assuming 'Grids' is a FeatureCollection in your Earth Engine assets
+# Grids = ee.FeatureCollection('YOUR_GRIDS_FEATURE_COLLECTION_PATH')  # Replace with your actual path
+
+# changing so that you can pass in the EE grid objects 
+
+
+def getNAIP(gridArea, year):
+    # """Input: A feature object. Output: An ee.Image (NAIP imagery mosaic cropped to the bounds of the input feature)."""
+    region = gridArea.geometry()
+    naip = geemap.get_annual_NAIP(year).filterBounds(gridArea).mosaic().clip(region).select(['R', 'G', 'B', 'N'])
+    return naip
+
+
+# Methods
+def rectanglify(gridArea):
+    """Input: A feature object. Output: An ee.FeatureCollection containing 8 features (vertical slices)."""
+    bounds = gridArea.geometry().bounds()
+    coords = ee.List(bounds.coordinates().get(0))
+    lowerLeft = ee.List(coords.get(0))
+    lowerRight = ee.List(coords.get(1))
+    upperRight = ee.List(coords.get(2))
+    minX = ee.Number(lowerLeft.get(0))
+    minY = ee.Number(lowerLeft.get(1))
+    maxX = ee.Number(lowerRight.get(0))
+    maxY = ee.Number(upperRight.get(1))
+    width = maxX.subtract(minX)
+
+    def create_rectangle(i):
+        i = ee.Number(i)
+        xStart = minX.add(width.multiply(i).divide(8))
+        xEnd = minX.add(width.multiply(i.add(1)).divide(8))
+        rectangle = ee.Geometry.Rectangle([xStart, minY, xEnd, maxY])
+        return ee.Feature(rectangle, {})
+
+    rectFeatures = ee.FeatureCollection(ee.List.sequence(0, 7).map(create_rectangle))
+    return rectFeatures
+
+def sliceNAIP(image, slicesFeatureCollection):
+    """Input: a NAIP image and a FeatureCollection of rectangle slices. Output: an ImageCollection where each image is the NAIP image clipped to one slice."""
+    rectList = slicesFeatureCollection.toList(slicesFeatureCollection.size())
+
+    def clip_image(rectFeature):
+        rectFeature = ee.Feature(rectFeature)
+        geom = rectFeature.geometry()
+        return image.clip(geom)
+
+    slices = rectList.map(clip_image)
+    return ee.ImageCollection(slices)
+
+def cumsumArray(arr):
+    """Calculates the cumulative sum of an ee.Array."""
+    arrList = arr.toList()
+
+    def cumsum_iter(x, acc):
+        acc = ee.List(acc)
+        last = ee.Number(ee.Algorithms.If(acc.size().gt(0), acc.get(acc.size().subtract(1)), 0))
+        return acc.add(ee.Number(x).add(last))
+
+    cumsumList = ee.List(arrList).iterate(cumsum_iter, ee.List([]))
+    return ee.Array(cumsumList)
+
+# Histogram Matching Functions
+def matchBand(sourceImage, referenceImage, bandName):
+    """Matches the histogram of a single band."""
+    sourceGeom = sourceImage.geometry().bounds()
+    refGeom = referenceImage.geometry().bounds()
+
+    sourceHist = sourceImage.reduceRegion(
+        reducer=ee.Reducer.fixedHistogram(0, 255, 256),
+        geometry=sourceGeom,
+        scale=30,
+        maxPixels=1e9,
+        bestEffort=True
+    )
+
+    referenceHist = referenceImage.reduceRegion(
+        reducer=ee.Reducer.fixedHistogram(0, 255, 256),
+        geometry=refGeom,
+        scale=30,
+        maxPixels=1e9,
+        bestEffort=True
+    )
+
+    sourceCounts = ee.Array(sourceHist.get(bandName)).slice(1, 1, 2).project([0])
+    referenceCounts = ee.Array(referenceHist.get(bandName)).slice(1, 1, 2).project([0])
+    sourceSum = ee.Number(sourceCounts.reduce(ee.Reducer.sum(), [0]).get([0]))
+    referenceSum = ee.Number(referenceCounts.reduce(ee.Reducer.sum(), [0]).get([0]))
+    sourceCDF = cumsumArray(sourceCounts.divide(sourceSum))
+    referenceCDF = cumsumArray(referenceCounts.divide(referenceSum))
+    sourceCDFList = ee.List(sourceCDF.toList())
+    referenceCDFList = ee.List(referenceCDF.toList())
+
+    def create_lookup(i):
+        i = ee.Number(i)
+        p = ee.Number(sourceCDFList.get(i))
+        differences = referenceCDFList.map(lambda r: ee.Number(r).subtract(p).abs())
+        minDiff = ee.Number(differences.sort().get(0))
+        matchedIndex = ee.Number(differences.indexOf(minDiff))
+        return matchedIndex
+
+    lookupTable = ee.List.sequence(0, 255).map(create_lookup)
+    return sourceImage.select(bandName).remap(ee.List.sequence(0, 255), lookupTable).rename(bandName)
+
+def matchAllBands(sourceImage, referenceImage, bandNames):
+    """Matches histograms for all specified bands."""
+    images = [matchBand(sourceImage, referenceImage, band) for band in bandNames]
+    return ee.Image.cat(images)
+
+def sampleImage(image, samplePoints, fileDesc):
+    """Samples an image and exports the results to Google Drive."""
+    samples = image.sampleRegions(
+        collection=samplePoints,
+        scale=1
+    )
+    task = ee.batch.Export.table.toDrive(
+        collection=samples,
+        description=fileDesc,
+        fileFormat='csv',
+        folder='Agroforestry'
+    )
+    task.start()
+    print(f'Export task {fileDesc} started.')
+
+
+# import ee
+
+# ee.Initialize()
+
+# Assuming getGridArea, getNAIP, rectanglify, sliceNAIP, matchAllBands, and sampleImage
+# are already defined in your Python script (as in the previous response).
+
+def matchSelf(gridArea, year, verbose=False, sample=False):
+    """Processes grids by matching each grid's histogram to itself."""
+    mosaics = []
+    gridFeatures = []
+
+    # for id in gridIDs:
+    if verbose:
+        print(f"Processing grid (matchSelf): {id}")
+    # gridArea = getGridArea(id)
+    gridFeatures.append(gridArea)
+    if verbose:
+        print(f'Map.addLayer(gridArea, {{}}, "{id} region")') #replace with desired python mapping library
+    naipImage = getNAIP(gridArea, year)
+    if verbose:
+        print(f'Map.addLayer(naipImage, {{}}, "{id} NAIP imagery")')#replace with desired python mapping library
+    rectFeatures = rectanglify(gridArea)
+    if verbose:
+        print(f'Map.addLayer(rectFeatures, {{color: "red"}}, "{id} divided region")')#replace with desired python mapping library
+    slicesCollection = sliceNAIP(naipImage, rectFeatures)
+    if verbose:
+        print(f'Map.addLayer(slicesCollection, {{}}, "{id} NAIP slices")')#replace with desired python mapping library
+    gridSamplePoints = ee.FeatureCollection.randomPoints(region=gridArea.geometry(), points=100, seed=5)
+    if sample:
+        sampleImage(naipImage, gridSamplePoints, f"{id}OriginalNAIPSampling")
+    if sample:
+        print(f'Map.addLayer(gridSamplePoints, {{color: "green"}}, "{id} Grid Sample Points")') #replace with desired python mapping library
+        print(gridSamplePoints, f"{id} Grid Sample Points")
+    origSlicesList = slicesCollection.toList(slicesCollection.size())
+    numSlices = slicesCollection.size().getInfo()
+    sliceSamples = []
+    for i in range(numSlices):
+        slice_img = ee.Image(origSlicesList.get(i))
+        sliceGeom = slice_img.geometry()
+        sliceSamplePoints = ee.FeatureCollection.randomPoints(region=sliceGeom, points=100, seed=5)
+        sliceSamples.append(sliceSamplePoints)
+        if sample:
+            sampleImage(slice_img, sliceSamplePoints, f"{id}OriginalSlice_{i}Sampling")
+        if sample:
+            print(f'Map.addLayer(sliceSamplePoints, {{color: "orange"}}, "{id} Original Slice {i} Sample Points")') #replace with desired python mapping library
+            print(sliceSamplePoints, f"{id} Original Slice {i} Sample Points")
+    referenceSlice = ee.Image(origSlicesList.get(numSlices - 1))
+    bands = ['R', 'G', 'B', 'N']
+    def correct_slice(slice_img):
+        return matchAllBands(slice_img, referenceSlice, bands)
+    correctedSlices = slicesCollection.map(correct_slice)
+    correctedMosaic = correctedSlices.mosaic()
+    if verbose:
+        print(f'Map.addLayer(correctedMosaic, {{}}, "{id} Corrected NAIP")') #replace with desired python mapping library
+    if sample:
+        sampleImage(correctedMosaic, gridSamplePoints, f"{id}HistogramMatchSampling")
+    mosaics.append(correctedMosaic)
+    unionGeom = ee.FeatureCollection(gridFeatures).geometry()
+    mosaicImage = ee.ImageCollection(mosaics).mosaic().clip(unionGeom)
+    return mosaicImage
+
+
+
+def matchGrid(gridIDs, referenceImage, year, verbose=False, sample=False):
+    """Matches grids in gridIDs to a reference mosaic."""
+    mosaics = []
+    gridFeatures = []
+
+    refImage = ee.Image(referenceImage)
+    if verbose:
+        print("Reference image for matching (entire corrected mosaic):", refImage)
+        print(f'Map.addLayer(refImage, {{min: 0, max: 255}}, "Reference Mosaic")')  # replace with desired python mapping library
+
+    for id in gridIDs:
+        if verbose:
+            print(f"Processing grid (matchGrid): {id}")
+
+        gridArea = getGridArea(id)
+        gridFeatures.append(gridArea)
+        if verbose:
+            print(f'Map.addLayer(gridArea, {{}}, "{id} region")')  # replace with desired python mapping library
+
+        naipImage = getNAIP(gridArea, year)
+        if verbose:
+            print(f'Map.addLayer(naipImage, {{}}, "{id} NAIP imagery")')  # replace with desired python mapping library
+
+        rectFeatures = rectanglify(gridArea)
+        if verbose:
+            print(f'Map.addLayer(rectFeatures, {{color: "red"}}, "{id} divided region")')  # replace with desired python mapping library
+
+        slicesCollection = sliceNAIP(naipImage, rectFeatures)
+        if verbose:
+            print(f'Map.addLayer(slicesCollection, {{}}, "{id} NAIP slices")')  # replace with desired python mapping library
+
+        gridSamplePoints = ee.FeatureCollection.randomPoints(region=gridArea.geometry(), points=100, seed=5)
+        if sample:
+            sampleImage(naipImage, gridSamplePoints, f"{id}OriginalNAIPSampling")
+        if sample:
+            print(f'Map.addLayer(gridSamplePoints, {{color: "green"}}, "{id} Grid Sample Points")')  # replace with desired python mapping library
+            print(gridSamplePoints, f"{id} Grid Sample Points")
+
+        origSlicesList = slicesCollection.toList(slicesCollection.size())
+        numSlices = slicesCollection.size().getInfo()
+        sliceSamples = []
+        for i in range(numSlices):
+            slice_img = ee.Image(origSlicesList.get(i))
+            sliceGeom = slice_img.geometry()
+            sliceSamplePoints = ee.FeatureCollection.randomPoints(region=sliceGeom, points=100, seed=5)
+            sliceSamples.append(sliceSamplePoints)
+            if sample:
+                sampleImage(slice_img, sliceSamplePoints, f"{id}OriginalSlice_{i}Sampling")
+            if sample:
+                print(f'Map.addLayer(sliceSamplePoints, {{color: "orange"}}, "{id} Original Slice {i} Sample Points")')  # replace with desired python mapping library
+                print(sliceSamplePoints, f"{id} Original Slice {i} Sample Points")
+
+        bands = ['R', 'G', 'B', 'N']
+
+        def correct_slice(slice_img):
+            return matchAllBands(slice_img, refImage, bands)  # Use refImage here
+
+        correctedSlices = slicesCollection.map(correct_slice)
+
+        correctedMosaic = correctedSlices.mosaic()
+        if verbose:
+            print(f'Map.addLayer(correctedMosaic, {{}}, "{id} Corrected NAIP")')  # replace with desired python mapping library
+        if sample:
+            sampleImage(correctedMosaic, gridSamplePoints, f"{id}HistogramMatchSampling")
+
+        mosaics.append(correctedMosaic)
+
+    unionGeom = ee.FeatureCollection(gridFeatures).geometry()
+    mosaicImage = ee.ImageCollection(mosaics).mosaic().clip(unionGeom)
+    return mosaicImages
